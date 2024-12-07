0f67f31b40796562696b719c05d70822db07c5f3
Vladyslav Hrieshnykh <vladyslav.hrieshnykh@student.tuke.sk>
2024-12-07 11:52:15 +0100

    Created view for main screen

--- MobileAttendanceApp/View/MainScreen/MainScreenView.swift
+++ MobileAttendanceApp/View/MainScreen/MainScreenView.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct MainScreenView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Button(action: {
+                    print("user profile icon check")
+                }, label: {
+                    Image(systemName: "person.crop.circle")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(.blue)
+                        .padding(.leading, 20)
+                })
+                Spacer()
+            }
+            Spacer()
+            
+            Button(action: {
+                print("tap to scan check")
+            }) {
+                Text("Tap to Scan")
+                    .font(.title)
+                    .foregroundColor(.blue)
+                    .padding(.bottom, 80)
+            }
+            Spacer()
+        }
     }
 }
 
