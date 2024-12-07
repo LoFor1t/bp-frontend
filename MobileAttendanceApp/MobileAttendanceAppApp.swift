d93aa2b9271feae5ac920f93c8c4b76dd2e145e8
Vladyslav Hrieshnykh <vladyslav.hrieshnykh@student.tuke.sk>
2024-12-07 11:36:56 +0100

    Initial Commit

---
+++ MobileAttendanceApp/MobileAttendanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileAttendanceAppApp.swift
+//  MobileAttendanceApp
+//
+//  Created by Vladyslav Hrieshnykh on 07/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MobileAttendanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
