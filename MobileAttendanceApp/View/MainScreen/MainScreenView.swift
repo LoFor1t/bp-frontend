42c0721207a4724ba7c403394a019a7cf9cca5e3
Vladyslav Hrieshnykh <vladyslav.hrieshnykh@student.tuke.sk>
2024-12-07 11:50:23 +0100

    Created view for main screen

---
+++ MobileAttendanceApp/View/MainScreen/MainScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  MainScreenView.swift
+//  MobileAttendanceApp
+//
+//  Created by Vladyslav Hrieshnykh on 07/12/2024.
+//
+
+import SwiftUI
+
+struct MainScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainScreenView()
+}
